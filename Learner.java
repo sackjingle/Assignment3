6033b5b5c4964db7f6a5b1971a8a1b39dce6a456
jordaneast <z3462985@unsw.edu.au>
2015-05-23 17:01:04 +1000

    change

--- Learner.java
+++ Learner.java
@@ -46,25 +46,35 @@ public class Learner {
 			updateSouth(input);
 		}
 	}
+//	private void updateBoard(char[][] input){
+//
+//		System.out.println("player is : '"+board[curY][curX]+"'");
+//		System.out.println("behind player is : '"+board[curY+1][curX]+"'");
+//		int x0 = curX - 2;
+//		int y0 = curY - 2;
+//		
+//		for (int i = 0; i < 5; i++) {
+//			for(int j = 0; j< 5; j++){
+//				System.out.println("added "+ input[0][i]);
+//				board[y0+i][x0+j] = input[i][j];
+//			}
+//		}
+//		board[curY][curX] = 'P';
+//		printBoard();
+//	}
 	
 	private void updateEast(char[][] input){
 		int x0 = curX - 2;
 		int y0 = curY - 2;
 		
 		for (int i = 0; i < 5; i++) {
-			System.out.println("added "+ input[i][4]);
-			board[y0+i][x0+4] = input[i][4];
+			System.out.println("added "+ input[0][i]);
+			board[y0+i][x0+4] = input[i][0];
 		}
-<<<<<<< HEAD
+
 		board[curY][curX] = ' ';
 		curX++;
 		board[curY][curX] = 'P';
-=======
-		board[curX][curY] = ' ';
-		curX++;
-		board[curX][curY] = 'P';
-		printBoard();
->>>>>>> origin/master
 		printBoard();
 	}
 	
@@ -73,22 +83,12 @@ public class Learner {
 		int y0 = curY - 2;
 		
 		for (int i = 0; i < 5; i++) {
-<<<<<<< HEAD
-			System.out.println("added "+ input[i][0]);
+			System.out.println("added "+ input[0][i]);
 			board[y0+i][x0] = input[i][0];
 		}
 		board[curY][curX] = ' ';
 		curX--;
 		board[curY][curX] = 'P';
-=======
-			System.out.println("added "+ input[i][4]);
-			board[x][y+i] = input[i][4];
-		}
-		
-		board[curX][curY] = ' ';
-		curX--;
-		board[curX][curY] = 'P';
->>>>>>> origin/master
 		printBoard();
 	}
 	
@@ -112,29 +112,16 @@ public class Learner {
 	}
 	
 	private void updateSouth(char[][] input){
-<<<<<<< HEAD
 		int x0 = curX - 2;
 		int y0 = curY - 2;
 		
 		for (int i = 0; i < 5; i++) {
-			System.out.println("added "+ input[4][i]);
+			System.out.println("added "+ input[0][i]);
 			board[y0+4][x0+i] = input[4][i];
 		}
 		board[curY][curX] = ' ';
 		curY++;
 		board[curY][curX] = 'P';
-=======
-		int x = curX - 2;
-		int y = curY + 3;
-		
-		for (int i = 0; i < 5; i++) {
-			System.out.println("added "+ input[i][4]);
-			board[x+i][y] = input[i][4];
-		}
-		board[curX][curY] = ' ';
-		curY--;
-		board[curX][curY] = 'P';
->>>>>>> origin/master
 		printBoard();
 	}
 	
