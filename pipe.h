26f8a57f183cec3cc1f04d81b7e4ab1811399d57
Jack Single <sackjingle@optusnet.com.au>
2015-05-23 13:16:32 +1000

    laters

--- pipe.h
+++
@@ -1,8 +0,0 @@
-/*********************************************
- *  pipe.h
- *  Socket Interface for Text-Based Adventure Game
- *  COMP3411 Artificial Intelligence
- *  UNSW Session 1, 2012
-*/
-
-int tcpopen(char *host, int port);
