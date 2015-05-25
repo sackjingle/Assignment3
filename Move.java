e494afe624a49e8d4f714605d33434bcef76e646
jordaneast <z3462985@unsw.edu.au>
2015-05-25 15:19:15 +1000

    move class

---
+++ Move.java
@@ -0,0 +1,21 @@
+
+public class Move {
+	private int x;
+	private int y;
+	private char move;
+	
+	public Move(int x,int y,char move){
+		this.x = x;
+		this.y = y;
+		this.move = move;
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
+	}
+	public int getMove(){
+		return move;
+	}
+}
